32e884ddfe63fadcfdf258b38b979e8b72d55bc0
iainm <terramirabilis1>
2020-01-03 12:33:13 +0000

    Add +3 Coal effect.

--- CWON_Tower_Bridge/Gameplay/Tower_Bridge_Gameplay.sql
+++ CWON_Tower_Bridge/Gameplay/Tower_Bridge_Gameplay.sql
@@ -1,11 +1,22 @@
 INSERT INTO Types (Type, Kind) VALUES ('BUILDING_TOWER_BRIDGE', 'KIND_BUILDING');
 
-INSERT INTO Buildings (BuildingType, Name, Description, PrereqTech, Cost, AdvisorType, MaxWorldInstances, IsWonder, RequiresPlacement, MustNotBeLake, MustBeAdjacentLand, Entertainment, Quote)
-VALUES ('BUILDING_TOWER_BRIDGE', 'LOC_BUILDING_TOWER_BRIDGE_NAME', 'LOC_BUILDING_TOWER_BRIDGE_DESCRIPTION', 'TECH_COMBUSTION', '1620', 'ADVISOR_GENERIC', '1', '1', '1', '1', '1', '3', 'LOC_BUILDING_TOWER_BRIDGE_QUOTE');
+INSERT INTO Buildings (BuildingType, Name, Description, PrereqTech, Cost, AdvisorType, MaxWorldInstances, IsWonder, RequiresPlacement, MustNotBeLake, MustBeAdjacentLand, Quote)
+VALUES ('BUILDING_TOWER_BRIDGE', 'LOC_BUILDING_TOWER_BRIDGE_NAME', 'LOC_BUILDING_TOWER_BRIDGE_DESCRIPTION', 'TECH_COMBUSTION', '1620', 'ADVISOR_GENERIC', '1', '1', '1', '1', '1', 'LOC_BUILDING_TOWER_BRIDGE_QUOTE');
 
 INSERT INTO Buildings_XP2 (BuildingType, Bridge) VALUES ('BUILDING_TOWER_BRIDGE', '1');
 
 INSERT INTO Building_ValidTerrains (BuildingType, TerrainType) VALUES ('BUILDING_TOWER_BRIDGE', 'TERRAIN_COAST');
+
+
+--Awards +3 Coal per turn
+INSERT INTO Modifiers(ModifierId, ModifierType)
+VALUES	('TOWER_BRIDGE_GRANT_COAL_PER_TURN',	'MODIFIER_SINGLE_CITY_ADJUST_FREE_RESOURCE_EXTRACTION');
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('TOWER_BRIDGE_GRANT_COAL_PER_TURN', 'ResourceType', 'RESOURCE_COAL');
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('TOWER_BRIDGE_GRANT_COAL_PER_TURN', 'Amount', '3');
+
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES ('BUILDING_TOWER_BRIDGE', 'TOWER_BRIDGE_GRANT_COAL_PER_TURN');
+
 
 -- +10% Production in cities on Capital Continent and +10% Gold in cities not on Capital Continent
 INSERT INTO Requirements (RequirementId, RequirementType) VALUES ('REQUIRES_CITY_IS_OWNER_CAPITAL_CONTINENT', 'REQUIREMENT_CITY_IS_OWNER_CAPITAL_CONTINENT');
